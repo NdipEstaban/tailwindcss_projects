220a8806cab241422c50a8ce78488183bef5be57
NdipEstaban <estabannd@gmail.com>
2023-01-15 00:35:02 +0100

    qr_code_component using tailwind css

---
+++ qr_code_component/tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screens:{
+      xs: {'max': '400px'},
+      sm:'480px',
+      md:'768px',
+      lg:'976px',
+      xl: '1440px'
+    },
+    colors:{
+      blue:"#3685FE",
+      grayishBlue:"#D5E1EF",
+      white:"#FFFFFF"
+    },
+    extend: {},
+  },
+  plugins: [],
+}
